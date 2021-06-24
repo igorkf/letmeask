080e26585d607beaceaa9dea448666ebf73ff9d1
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-24 09:49:51 -0300

    added room code and styles

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,15 +1,25 @@
+import { useParams } from 'react-router'
+
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
 
 import '../styles/room.scss'
+import '../styles/room-code.scss'
+
+type RoomParams = {
+  id: string
+}
 
 export function Room() {
+  const params = useParams<RoomParams>()
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
